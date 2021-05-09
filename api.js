6db90f49d48f170167861f8acddbe31c8e4c905e
MadMonkey87 <myself@philippewechsler.ch>
2021-05-09 22:39:16 +0200

    remove cache item

--- api.js
+++ api.js
@@ -29,6 +29,15 @@ module.exports = {
 	async getCacheItem({ homey, params, query, body }) {
 		return homey.app.getCacheItem(query.name);
 	},
+	async removeCacheItem({ homey, params, query, body }) {
+		await homey.app.removeCacheItem(query.name, (err, result) => {
+			return {
+				success: err == null,
+				error: err,
+				result: result
+			};
+		})
+	},
 	async clearCache({ homey, params, query, body }) {
 		await homey.app.clearCache((err, result) => {
 			return {
